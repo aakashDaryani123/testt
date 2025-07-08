4b9b730796e6d7e607eff6822997c73412b374a2
mail2rakeshaggarwal@gmail.com <mail2rakeshaggarwal@gmail.com>
2025-07-08 22:46:03 +0530

    go

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.get("/", async (req, res) => {
   const browser = await puppeteer.launch({
+    headless : true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();

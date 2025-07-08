c951f98b1e82c97c6d8dd4c4d936fb939c68b5d6
mail2rakeshaggarwal@gmail.com <mail2rakeshaggarwal@gmail.com>
2025-07-08 23:25:42 +0530

    dockr added

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const puppeteer = require("puppeteer");
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
+const scrapeLogic = async (res) => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [  "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+
+  try {
+    const page = await browser.newPage();
+    await page.goto("https://example.com");
+    const text = await page.$eval("h1", (el) => el.textContent);
+    res.send(text);
+  } catch (err) {
+    res.status(500).send("Error during scraping.");
+    console.error(err);
+  } finally {
+    await browser.close();
+  }
+};
 
 app.get("/", async (req, res) => {
-  const browser = await puppeteer.launch({
-    headless : true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-  await page.goto("https://example.com");
-  const text = await page.$eval("h1", (el) => el.textContent);
-  res.send(text);
-
-  await browser.close();
+  await scrapeLogic(res);
 });
 
 app.listen(process.env.PORT || 3000, () => {
